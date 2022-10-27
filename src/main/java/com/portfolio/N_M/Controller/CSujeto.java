7211cf39901acf824bb2da979d6b26fb47d3905e
chevybmw <mmnatiello@gmail.com>
2022-10-27 12:23:35 -0300

    actualizado http method

--- src/main/java/com/portfolio/N_M/Controller/CSujeto.java
+++ src/main/java/com/portfolio/N_M/Controller/CSujeto.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +33,7 @@ public class CSujeto {
     
     SSujeto sSujeto;
     
-   
+    
     @GetMapping("/lista")
     public ResponseEntity<List<Sujeto>> list(){
         List<Sujeto> list = sSujeto.list();
--- src/main/java/com/portfolio/N_M/Security/MainSecurity.java
+++ src/main/java/com/portfolio/N_M/Security/MainSecurity.java
@@ -11,6 +11,7 @@ import com.portfolio.N_M.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -46,6 +47,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET)                  
+                 .permitAll() 
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
